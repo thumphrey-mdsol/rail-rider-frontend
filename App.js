54f430b08ac8867adc58fbc702df2c9326d07a51
github username <github email address>
2020-04-06 16:47:20 -0400

    kinda working

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
-import AppNavigator from './src/navigator/AppNavigator';
+import Login from './src/screens/Login';
+import SignUp from './src/screens/SignUp';
 
 export default function App() {
   return (
-      <AppNavigator />
+    
+    <View style={styles.container}>
+      <StatusBar backgroundColor="1c313a" barStyle="light-content"/>
+      <Login />
+    </View>
+
   );
 }
 
